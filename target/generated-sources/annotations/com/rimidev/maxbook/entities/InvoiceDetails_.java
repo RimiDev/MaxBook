9ed3d200ff5a2d2f3e3b8eab61d2226fc51da26f
plang <plang@LAPTOP-Q9J4FH33.local>
2018-02-06 17:18:56 -0500

    Working on the All Books page

--- target/generated-sources/annotations/com/rimidev/maxbook/entities/InvoiceDetails_.java
+++ target/generated-sources/annotations/com/rimidev/maxbook/entities/InvoiceDetails_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-31T14:51:17")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-06T16:33:07")
 @StaticMetamodel(InvoiceDetails.class)
 public class InvoiceDetails_ { 
 
