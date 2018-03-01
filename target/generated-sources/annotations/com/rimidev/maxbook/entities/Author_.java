4b7fb0ae835b69c1144cc3be8e6351a0bbd998c4
Maxime Lacasse <maximelacassedeveloper@gmail.com>
2018-03-01 17:14:44 -0500

    Advanced Search is totally functional

--- target/generated-sources/annotations/com/rimidev/maxbook/entities/Author_.java
+++ target/generated-sources/annotations/com/rimidev/maxbook/entities/Author_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.1.v20171221-rNA", date="2018-02-27T18:14:38")
+@Generated(value="EclipseLink-2.7.1.v20171221-rNA", date="2018-03-01T15:23:51")
 @StaticMetamodel(Author.class)
 public class Author_ { 
 
--- target/generated-sources/annotations/com/rimidev/maxbook/entities/Review_.java
+++ target/generated-sources/annotations/com/rimidev/maxbook/entities/Review_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.1.v20171221-rNA", date="2018-02-27T18:14:38")
+@Generated(value="EclipseLink-2.7.1.v20171221-rNA", date="2018-03-01T15:23:51")
 @StaticMetamodel(Review.class)
 public class Review_ { 
 
