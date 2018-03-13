c70eaccf5d5af34e272b24c9dff6b9d9201745fe
Philippe Langlois <planglois.pedroso@gmail.com>
2018-03-13 18:44:00 -0400

    Help page and associated messages in bundle added.

--- target/generated-sources/annotations/com/rimidev/maxbook/entities/Review_.java
+++ target/generated-sources/annotations/com/rimidev/maxbook/entities/Review_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.1.v20171221-rNA", date="2018-03-07T15:31:01")
+@Generated(value="EclipseLink-2.7.1.v20171221-rNA", date="2018-03-13T17:00:32")
 @StaticMetamodel(Review.class)
 public class Review_ { 
 
