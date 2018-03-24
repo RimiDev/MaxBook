c4dee9ad37525fb2c5cb56f1b6b8cbd04ac14f29
Philippe Langlois <planglois.pedroso@gmail.com>
2018-03-24 17:07:01 -0400

    Added edit and add code for survey management but missing delete

--- target/generated-sources/annotations/com/rimidev/maxbook/entities/InvoiceDetails_.java
+++ target/generated-sources/annotations/com/rimidev/maxbook/entities/InvoiceDetails_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.1.v20171221-rNA", date="2018-03-22T20:11:56")
+@Generated(value="EclipseLink-2.7.1.v20171221-rNA", date="2018-03-24T13:36:16")
 @StaticMetamodel(InvoiceDetails.class)
 public class InvoiceDetails_ { 
 
