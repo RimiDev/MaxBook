35cc6dc2c29fd637dd65736366bd706622b19fa1
ericbehughes <e.hughes92@icloud.com>
2018-03-27 18:39:08 -0400

    file upload works

--- src/main/java/com/rimidev/maxbook/beans/UploadPage.java
+++ src/main/java/com/rimidev/maxbook/beans/UploadPage.java
@@ -16,6 +16,7 @@ import javax.faces.validator.ValidatorException;
 import javax.servlet.http.Part;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Serializable;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,7 +25,9 @@ import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.component.UIViewRoot;
+import javax.faces.context.ExternalContext;
 import javax.imageio.ImageIO;
 import org.primefaces.model.UploadedFile;
 
@@ -32,88 +35,32 @@ import org.primefaces.model.UploadedFile;
  *
  * @author 1513733
  */
-
 @ManagedBean
-@ViewScoped
-public class UploadPage {
+@SessionScoped
+public class UploadPage implements Serializable {
 
-    private Part uploadedFile;
-    private String fileContent;
-    private UploadedFile file;
+  private Part uploadedFile;
+  private String folder = "c:\\files";
 
-    public void validateFile(FacesContext ctx, UIComponent comp, Object value) {
-        List<FacesMessage> msgs = new ArrayList<FacesMessage>();
-        Part file = (Part) value;
-        if (file.getSize() > 1024) {
-            msgs.add(new FacesMessage("file too big"));
-        }
-        if (!"text/plain".equals(file.getContentType())) {
-            msgs.add(new FacesMessage("not a text file"));
-        }
-        if (!msgs.isEmpty()) {
-            throw new ValidatorException(msgs);
-        }
+  public Part getUploadedFile() {
+    return uploadedFile;
+  }
+
+  public void setUploadedFile(Part uploadedFile) {
+    this.uploadedFile = uploadedFile;
+  }
+
+  public void saveFile() {
+
+    try (InputStream input = uploadedFile.getInputStream()) {
+      ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+
+      folder = ec.getRealPath("/");
+      
+      Files.copy(input, new File(folder, "978-0060256654" + ".jpg").toPath());
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
-    public void uploadFile(String isbn) throws IOException {
-        Path file = Files.createTempFile(Paths.get("resources/images/books"), isbn, ".jpg");
-        try (InputStream input = uploadedFile.getInputStream()) {
-//            fileContent = new Scanner(uploadedFile.getInputStream()).useDelimiter("\\A").next()
-            Files.copy(input, file, StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                    "error uploading file",
-                    null);
-            FacesContext.getCurrentInstance().addMessage(null, msg);
-        }
-
-        fileContent = file.getFileName().toString();
-    }
-
-    public void upload(String isbn) {
-        if (file != null) {
-            BufferedImage image = null;
-            try {
-                
-                URL url = new URL("http://www.mkyong.com/image/mypic.jpg");
-                image = ImageIO.read(url);
-                
-                ImageIO.write(image,"jpg",new File("resources/images/books/"+isbn+".jpg"));
-                
-            } catch (IOException e) {
-                FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                        "error uploading file",
-                        null);
-                FacesContext.getCurrentInstance().addMessage(null, msg);
-            }
-            
-            UIViewRoot rootView = FacesContext.getCurrentInstance().getViewRoot();
-            
-
-            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
-            FacesContext.getCurrentInstance().addMessage(null, message);
-
-        }
-    }
-
-    public Part getUploadedFile() {
-        return uploadedFile;
-    }
-
-    public void setUploadedFile(Part uploadedFile) {
-        this.uploadedFile = uploadedFile;
-    }
-
-    public String getFileContent() {
-        return fileContent;
-    }
-
-    public UploadedFile getFile() {
-        return file;
-    }
-
-    public void setFile(UploadedFile file) {
-        this.file = file;
-    }
+  }
 
 }
