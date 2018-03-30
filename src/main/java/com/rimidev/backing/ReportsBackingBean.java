1dba30c88c979696f72f13e27dbe3d3270ab3187
ericbehughes <e.hughes92@icloud.com>
2018-03-30 18:15:26 -0400

    from and to dates work for invoice details sales

--- src/main/java/com/rimidev/backing/ReportsBackingBean.java
+++ src/main/java/com/rimidev/backing/ReportsBackingBean.java
@@ -16,7 +16,10 @@ import com.rimidev.maxbook.entities.Publisher;
 import com.rimidev.maxbook.entities.Survey;
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
@@ -26,6 +29,7 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import org.primefaces.event.SelectEvent;
 
 /**
  * manager can request a number of reports on the inventory, orders and clients.
@@ -47,7 +51,7 @@ public class ReportsBackingBean implements Serializable {
 
   @Inject
   InvoiceDetailsJpaController invoiceDetailsJpaController;
-  
+
   private Date fromDate;
   //private double totalSalesValue;
   private List<Object[]> clients;
@@ -55,7 +59,7 @@ public class ReportsBackingBean implements Serializable {
   private List<Object[]> publishers;
   private List<Invoice> invoicesList;
 
-  private List<Object[]> totalSalesList;
+  private List<InvoiceDetails> totalSalesList;
   private List<Object[]> filteredClients;
   private List<Object[]> filteredAuthors;
 
@@ -72,7 +76,6 @@ public class ReportsBackingBean implements Serializable {
 //    }
 //    return totalSalesValue;
 //  }
-
   public Date getFromDate() {
     return fromDate;
   }
@@ -92,25 +95,30 @@ public class ReportsBackingBean implements Serializable {
 
   @PostConstruct
   public void init() {
+    Calendar cal = Calendar.getInstance();
+    Date today = cal.getTime();
+    cal.add(Calendar.YEAR, -1); // to get previous year add -1
+    Date lastYear = cal.getTime();
+    fromDate = lastYear;
+    toDate = new Date();
     totalSalesList = new ArrayList();
     clients = new ArrayList();
     authors = new ArrayList();
     publishers = new ArrayList();
-    fromDate = new Date();
     filteredClients = new ArrayList();
     filteredAuthors = new ArrayList();
     filteredPublishers = new ArrayList();
   }
 
-  public List<Object[]> getTotalInvoiceDetailSales() {
-    totalSalesList = invoiceDetailsJpaController.getTotalInvoiceDetailSales(new Date(), new Date());
+  public List<InvoiceDetails> getTotalInvoiceDetailSales() {
+    totalSalesList = invoiceDetailsJpaController.getTotalInvoiceDetailSales(fromDate, toDate);
 
     return totalSalesList;
   }
-  
-  public double getTotalSalesValue(){
-    double totalSalesValue=0;
-    invoicesList =  invcon.findInvoiceEntities();
+
+  public double getTotalSalesValue() {
+    double totalSalesValue = 0;
+    invoicesList = invcon.findInvoiceEntities();
     if (invoicesList != null) {
       for (Invoice sale : invoicesList) {
 
@@ -119,7 +127,7 @@ public class ReportsBackingBean implements Serializable {
       logger.log(Level.SEVERE, "total invoice sales value" + totalSalesValue);
     }
     return totalSalesValue;
-   
+
   }
 
   public List<Object[]> getSalesByClient() {
@@ -138,12 +146,12 @@ public class ReportsBackingBean implements Serializable {
     return clients = invoiceDetailsJpaController.getTotalSalesByPublisher(new Date(), new Date());
   }
 
-  public void setTotalSalesList(List<Object[]> totalSales) {
+  public void setTotalSalesList(List<InvoiceDetails> totalSales) {
     this.totalSalesList = totalSales;
   }
 
-  public void updateFromDate(Date d) {
-    this.fromDate = d;
+  public void updateFromDate(SelectEvent event) {
+    this.getTotalInvoiceDetailSales();
   }
 
   public List<Object[]> getClients() {
@@ -193,8 +201,8 @@ public class ReportsBackingBean implements Serializable {
   public void setFilteredPublishers(List<Object[]> filteredPublihsers) {
     this.filteredPublishers = filteredPublihsers;
   }
-  
-   public List<Invoice> getInvoicesList() {
+
+  public List<Invoice> getInvoicesList() {
     return invoicesList;
   }
 
